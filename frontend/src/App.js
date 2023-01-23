fae9608d417df47ab4ffd7c02aba44bdb5a1c3fa
Barti <bartosz.majkowski@wp.pl>
2023-01-23 22:51:06 +0100

    start page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import "./App.css"
+import React from "react";
+import {Route, Routes} from "react-router-dom"
+import Registration from "./components/Registration"
+import Login from "./components/Login"
+import Start from "./components/Start"
+
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    return (
+        <div className="flex flex-col">
+            <h1 className="mx-auto font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">&#120703; Game</h1>
+            <Routes>
+                <Route path="/" element={<Start/>}/>
+                {/*<Route path="/registration" element={<Registration/>}/>*/}
+                {/*<Route path="/login" element={<Login/>}/>*/}
+            </Routes>
+        </div>
+    )
 }
---
+++ frontend/src/components/Start.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Start = () => {
+    return (
+        <div className="flex flex-col text-center">
+            <button className="w-6/12 mx-auto bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                <Link to={'/registration'}>Registration</Link>
+            </button>
+            <button className="w-6/12 mx-auto bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                <Link to={'/login'}>Log in</Link>
+            </button>
+        </div>
+    )
+}
+
+export default Start
